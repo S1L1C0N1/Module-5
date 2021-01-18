7781a3fd079751c2c11b5f94acfbf94661bceaf7
S1L1C0N1 <55473783+S1L1C0N1@users.noreply.github.com>
2021-01-18 00:02:32 -0800

    Create dynomite_dicts.py

---
+++ dynomite_dicts.py
@@ -0,0 +1,12 @@
+def func():
+    pokedex = {}
+    dict = {'Name': 'venosaur', 'Class': 'grass, poison'}
+    dict = {'Name': 'charizard', 'Class': 'fire, flying'}
+    dict = {'Name': 'blastoise', 'Class': 'water'}
+    print("dict['Name']: ", dict['Name'])
+    print("dict['Class']: ", dict['Class'])
+
+
+
+if __name__ == "__main__":
+    func()
